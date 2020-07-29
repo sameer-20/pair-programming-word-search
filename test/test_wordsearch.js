ab50a5955e5fec79c98dcb7e7af539c6397bdaba
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-07-29 22:11:56 +0000

    Code for word search in a letter matrix

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,9 @@
+// Test file for wordsearch.js
+
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const { wordSearch } = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -35,4 +37,77 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+
+  it("should return true if the word is present in vertical direction", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'VEEPUU')
+
+    assert.isTrue(result);
+  });
+
+
+  it("should return false if the word is NOT present in vertical direction", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'EFMABC')
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is  present in horizontal backward direction", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ETJM')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is  NOT present in horizontal backward direction", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ETJMAA')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([], 'EFMABC')
+
+    assert.isFalse(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,61 @@
-const wordSearch = (letters, word) => { 
+// To find a word in a matrix by doing a search in horizontal, vertical and backward directions.
+
+// Search a word in horizontal direction
+
+const wordSearchHorizontal = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
+        
     }
+    return false;
 }
 
-module.exports = wordSearch
+// Search a word in Backward direction
+
+const wordSearchBackward = (letters, word) => { 
+    const horizontalJoinReverse = letters.map(ls => ls.reverse().join(''))
+    for (l of horizontalJoinReverse) {
+        if (l.includes(word)) return true        
+    }
+    return false;
+}
+
+// Do a transpose of the word matrix
+
+const transpose = function(matrix) {
+  
+    let temp = []; 
+  
+    for (let i = 0; i < matrix.length; i++) {    
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (!temp[j]) {
+          temp[j] = [];
+        }
+        temp[j][i] = matrix[i][j];                  
+      }    
+    }
+   return temp;
+  };
+
+// Do a word search in the following directions
+
+const wordSearch = (letters, word) => { 
+    if (wordSearchHorizontal(letters,word)) { // Horizontal search
+        return true;
+    }
+    else if (wordSearchBackward(letters,word)) { // Horizontal Backward search
+        return true;
+    }
+    else if (wordSearchHorizontal(transpose(letters),word)) { // Vertical search
+        return true;
+    }
+    else if (wordSearchBackward(transpose(letters),word)) { // Vertical Backward search
+        return true;
+    }
+
+    return false;    
+}
+
+
+module.exports = { wordSearch }; 
